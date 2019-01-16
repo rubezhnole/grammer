589fca4bb0e917ac30aacc3d039ad379b3399009
Andrey Kotlyarenko <a.kotlyarenko@spd-ukraine.com>
2019-01-16 16:05:29 +0200

    Init structure.

---
+++ src/main/java/com/kotl/grammer/GrammerApplication.java
@@ -0,0 +1,14 @@
+package com.kotl.grammer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrammerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrammerApplication.class, args);
+    }
+
+}
+
